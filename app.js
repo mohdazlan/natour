4bdf6cbd7bb80259a2a984084e12b70398084f62
azlan <mhdazlanabaziz@gmai.com>
2020-08-27 12:13:09 +0800

    basic crouting

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('hello world')
+// app.get('/', (req, res) => {
+//     res.status(200).send('hello world')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'berjaya',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 })
 
 const port = 3000
