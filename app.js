b099b4f885c7f5402a707a87b28957277a69b15a
azlan <mhdazlanabaziz@gmai.com>
2020-09-01 14:36:00 +0800

    implement pdelete

--- app.js
+++ app.js
@@ -80,6 +80,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     })
 })
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'gagal',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(204).json({
+        status: 'success',
+        maklumat:
+            null
+
+    })
+})
+
 const port = 3000
 app.listen(port, () => {
     console.log(`App is running on port ${port} ..`)
